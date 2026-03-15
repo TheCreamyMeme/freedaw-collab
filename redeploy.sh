1696c0bbb2137c955c9385ac2c05bd061a804020
Ben Noble <benjamintroynoble@gmail.com>
2026-03-15 16:15:26 -0600

    yeet

--- redeploy.sh
+++ redeploy.sh
@@ -1,10 +1,10 @@
 #/usr/bin/env bash
-git pull origin
-docker build --no-cache -t freedaw-api ./backend
-docker stop freedaw-api
-docker rm freedaw-api
-docker run -d --name freedaw-api -p 3010:3000 -v ./backend/webdaw-projects:/app/projects -v ./backend/webdaw-samples:/app/samples --restart unless-stopped freedaw-api
-docker build --no-cache -t freedaw ./frontend
-docker stop freedaw
-docker rm freedaw
+git pull origin &&
+docker build --no-cache -t freedaw-api -f ./backend/dockerfile ./backend &&
+docker stop freedaw-api &&
+docker rm freedaw-api &&
+docker run -d --name freedaw-api -p 3010:3000 -v ./backend/webdaw-projects:/app/projects -v ./backend/webdaw-samples:/app/samples --restart unless-stopped freedaw-api &&
+docker build --no-cache -t freedaw -f ./frontend/dockerfile ./frontend &&
+docker stop freedaw &&
+docker rm freedaw &&
 docker run -d --name freedaw -p 3009:3000 --restart unless-stopped free-daw:latest
